174bc2703741f17c7ed58cbc718b7f678371869b
hrgrigorov <72554567+hrgrigorov@users.noreply.github.com>
2021-10-22 11:01:33 +0300

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,20 @@
+class minecraft {
+  file { '/opt/minecraft':
+    ensure => directotry,
+  }
+  file { '/opt/minecraft/minecraft_server.jar':
+    ensure => file,
+    source => "https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
+  }
+  package { 'java':
+    ensure => present,
+  }
+  file { '/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file { '/etc/systemd/service/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}
