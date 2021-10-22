d4d6d2080cae73162d0067d90bb1b0ba156f005d
hrgrigorov <72554567+hrgrigorov@users.noreply.github.com>
2021-10-22 11:27:34 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,7 @@ class minecraft {
   }
   file { '/opt/minecraft/minecraft_server.jar':
     ensure => file,
-    source => "https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
+    source => 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
   }
   package { 'java':
     ensure => present,
